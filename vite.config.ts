97bd6292fd0c377cd73855841179c2a1e5e64561
Gio Justiniano <giomikee.20@gmail.com>
2025-07-11 08:50:47 +0200

    feat(GithubPages): setup for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 /// <reference types="vitest/config" />
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.VITE_APP_BASE || '/',
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-  }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: env.VITE_APP_BASE || '/',
+    plugins: [react()],
+    test: {
+      environment: 'jsdom',
+      globals: true,
+    }
+  };
 });
